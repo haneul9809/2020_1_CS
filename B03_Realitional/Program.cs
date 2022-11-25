e124ab7391f95beb3fc1ec595b7e738125301fbb
haneul9809 <67767912+haneul9809@users.noreply.github.com>
2022-11-25 12:29:20 +0900

    Add files via upload

---
+++ B03_Realitional/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B03_Realitional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            Console.Write("x를 입력하세요 : ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("y를 입력하세요 : ");
+            y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} > {1} = {2}", x, y, x > y);
+            Console.WriteLine("{0} >= {1} = {2}", x, y, x >= y);
+            Console.WriteLine("{0} < {1} = {2}", x, y, x < y);
+            Console.WriteLine("{0} <= {1} = {2}", x, y, x <= y);
+            Console.WriteLine("{0} == {1} = {2}", x, y, x == y);
+            Console.WriteLine("{0} != {1} = {2}", x, y, x != y);
+        }
+    }
+}
---
+++ B05_Loop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B05_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= 100; i++)
+                sum += i;
+            Console.WriteLine("1~100까지의 합 : {0}",sum);
+
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                    evenSum += i;//짝수
+                else
+                    oddSum += i;//홀수
+            }
+            Console.WriteLine("1~100까지의 짝수의 합 : {0}", evenSum);
+            Console.WriteLine("1~100까지의 홀수의 합 : {0}", oddSum);
+
+            double rSum = 0;
+            for (int i = 1; i <=100; i++)
+                rSum += 1.0 / i;
+            Console.WriteLine("1~100까지의 역수의 합 : {0}",rSum);
+
+
+        }
+    }
+}
